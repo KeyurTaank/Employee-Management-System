607b5653a5a44b8abbf70e549c8d923a42f93429
Keyur Tank <68388207+KeyurTaank@users.noreply.github.com>
2024-06-09 15:12:57 +0530

    Add files via upload

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,109 @@
+﻿using Employee_Management_System.Data;
+using Employee_Management_System.Models;
+using Employee_Management_System.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management_System.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public EmployeesController(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var employees =  await applicationDbContext.Employees.ToListAsync();
+            return View(employees);
+        }
+        
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeViewModel)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeViewModel.Name,
+                Email = addEmployeeViewModel.Email,
+                Salary = addEmployeeViewModel.Salary,
+                Department = addEmployeeViewModel.Department,
+                DateOfBirth = addEmployeeViewModel.DateOfBirth,
+            };
+
+            await applicationDbContext.Employees.AddAsync(employee);
+            await applicationDbContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> View(Guid id)
+        {
+            var employee = await applicationDbContext.Employees.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (employee != null)
+            {
+                var viewModel = new UpdateEmployeeViewModel()
+                {
+                    Id = employee.Id,
+                    Name = employee.Name,
+                    Email = employee.Email,
+                    Salary = employee.Salary,
+                    Department = employee.Department,
+                    DateOfBirth = employee.DateOfBirth,
+                };
+
+                return await Task.Run(() => View("View", viewModel));
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> View(UpdateEmployeeViewModel model)
+        {
+            var employee = await applicationDbContext.Employees.FindAsync(model.Id);
+
+            if (employee != null)
+            {
+                employee.Name = model.Name;
+                employee.Email = model.Email;
+                employee.Salary = model.Salary;
+                employee.Department = model.Department;
+                employee.DateOfBirth = model.DateOfBirth;
+
+                await applicationDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(UpdateEmployeeViewModel model)
+        {
+            var employee = await applicationDbContext.Employees.FindAsync(model.Id);
+            
+            if (employee != null)
+            {
+                applicationDbContext.Employees.Remove(employee);
+                await applicationDbContext.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Employee_Management_System.Models.Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Employee_Management_System.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Employee> Employees {  get; set; }
+    }
+}
